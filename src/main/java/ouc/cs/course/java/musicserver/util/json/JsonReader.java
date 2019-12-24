8899a1f96643f7f9f0994e798e5f4400f7f50975
刘阳 <249606097@qq.com>
2019-12-24 22:25:14 +0800

    start

--- src/main/java/ouc/cs/course/java/musicserver/util/json/JsonReader.java
+++ src/main/java/ouc/cs/course/java/musicserver/util/json/JsonReader.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import javax.servlet.http.HttpServletRequest;
 import net.sf.json.JSONObject;
 
@@ -11,7 +12,7 @@ public class JsonReader {
 	public static JSONObject receivePost(HttpServletRequest request) throws IOException, UnsupportedEncodingException {
 
 		// 读取请求内容
-		BufferedReader br = new BufferedReader(new InputStreamReader(request.getInputStream(), "utf-8"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(request.getInputStream(), StandardCharsets.UTF_8));
 		String line = null;
 		StringBuilder sb = new StringBuilder();
 		while ((line = br.readLine()) != null) {
